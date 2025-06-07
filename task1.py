d685b2623ac342cc8d23dae3da329812dceafe25
arpan1137 <arpanrooj1000@gmail.com>
2025-06-08 00:34:05 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+A=input('enter the first number')
+B=input('enter the second number')
+A=float(A)
+B=float(B)
+c=A+B
+d=A-B
+e=A*B
+f=A/B
+print("the sum is",c)
+print("the subtraction is",d)
+print("the multipliction is",e)
+print("the division is",f)
---
+++ task2.py
@@ -0,0 +1,15 @@
+# Step 1: Take user's first name and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Step 2: Concatenate first name and last name
+full_name = first_name + " " + last_name
+
+# Step 3: Print a personalized greeting message
+print("Hello, " + full_name + "! Welcome!")
+
+
+
+
+
+
